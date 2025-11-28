06e78406f2a10c19deb07bf31c8de85dad2344c0
tmack <angry.train3825@fastmail.com>
2025-11-28 18:40:09 -0500

    feat: afters moved from before to after

--- internal/middleware/before.go
+++ internal/middleware/before.go
@@ -54,29 +54,3 @@ func StructuredLoggingMiddleware(next http.Handler) http.Handler {
 		next.ServeHTTP(w, r)
 	})
 }
-
-// middleware for post processing (after the handler has completed)
-
-type loggingResponseWriter struct {
-	http.ResponseWriter
-	statusCode int
-}
-
-func NewLoggingResponseWriter(w http.ResponseWriter) *loggingResponseWriter {
-	return &loggingResponseWriter{w, http.StatusOK}
-}
-
-func (lrw *loggingResponseWriter) WriteHeader(code int) {
-	lrw.statusCode = code
-	lrw.ResponseWriter.WriteHeader(code)
-}
-
-func LoggingMiddleware(next http.Handler) http.Handler {
-	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		lrw := NewLoggingResponseWriter(w)
-		next.ServeHTTP(lrw, r)
-
-		reqLogger := logger.WithResponseInfo(r.Context(), lrw.statusCode)
-		reqLogger.Info("loggingMiddleware completed")
-	})
-}
