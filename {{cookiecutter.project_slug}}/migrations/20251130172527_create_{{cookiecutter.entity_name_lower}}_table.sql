387a33b2730c9503cdd38923302639154a05ad8b
tmack <angry.train3825@fastmail.com>
2025-12-01 17:29:44 -0500

    feat: db table is singular

--- {{cookiecutter.project_slug}}/migrations/20251130172527_create_{{cookiecutter.entity_name_lower}}_table.sql
+++ {{cookiecutter.project_slug}}/migrations/20251130172527_create_{{cookiecutter.entity_name_lower}}_table.sql
@@ -1,16 +1,16 @@
 -- +goose Up
 -- +goose StatementBegin
-CREATE TABLE {{cookiecutter.entity_name_lower}}s (
+CREATE TABLE {{cookiecutter.entity_name_lower}} (
     id UUID PRIMARY KEY,
     name VARCHAR(255) NOT NULL,
     created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
     updated_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
 );
 
-CREATE INDEX idx_{{cookiecutter.entity_name_lower}}s_name ON {{cookiecutter.entity_name_lower}}s(name);
+CREATE INDEX idx_{{cookiecutter.entity_name_lower}}_name ON {{cookiecutter.entity_name_lower}}(name);
 -- +goose StatementEnd
 
 -- +goose Down
 -- +goose StatementBegin
-DROP TABLE IF EXISTS {{cookiecutter.entity_name_lower}}s;
+DROP TABLE IF EXISTS {{cookiecutter.entity_name_lower}};
 -- +goose StatementEnd
