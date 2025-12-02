c96634bf33610fd30d24688a7a836038d4b4f437
tmack <angry.train3825@fastmail.com>
2025-12-02 08:51:21 -0500

    feat: tables are singular

--- {{cookiecutter.project_slug}}/internal/db/connect.go
+++ {{cookiecutter.project_slug}}/internal/db/connect.go
@@ -7,11 +7,16 @@ import (
 	"gorm.io/driver/postgres"
 	"gorm.io/driver/sqlite"
 	"gorm.io/gorm"
+	"gorm.io/gorm/schema"
 )
 
 func MakeDb(databaseURL string, log *slog.Logger) *gorm.DB {
 	// Initialize database connection
-	db, err := gorm.Open(postgres.Open(databaseURL), &gorm.Config{})
+	db, err := gorm.Open(postgres.Open(databaseURL), &gorm.Config{
+		NamingStrategy: schema.NamingStrategy{
+			SingularTable: true,
+		},
+	})
 	if err != nil {
 		log.Error("failed to connect to database", slog.String("error", err.Error()))
 		os.Exit(1)
@@ -21,6 +26,10 @@ func MakeDb(databaseURL string, log *slog.Logger) *gorm.DB {
 
 func MakeDbSqlite() (*gorm.DB, error) {
 	// Initialize database connection
-	db, err := gorm.Open(sqlite.Open(":memory:"), &gorm.Config{})
+	db, err := gorm.Open(sqlite.Open(":memory:"), &gorm.Config{
+		NamingStrategy: schema.NamingStrategy{
+			SingularTable: true,
+		},
+	})
 	return db, err
 }
